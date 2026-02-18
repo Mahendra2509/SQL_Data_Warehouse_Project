7ea61757e80a7e9b6a9a9ee8e22a9d5cbe6a5899
Naga Mahendra Polavarapu <mahendrachowdary36@gmail.com>
2026-02-18 20:51:32 +0530

    Update Views.sql

--- Scripts/Gold/Views.sql
+++ Scripts/Gold/Views.sql
@@ -15,13 +15,13 @@ Usage:
 */
 
 -- =============================================================================
--- Create Dimension: gold.dim_customers
+-- Create Dimension: Gold.dim_customers
 -- =============================================================================
-IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
-    DROP VIEW gold.dim_customers;
+IF OBJECT_ID('Gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW Gold.dim_customers;
 GO
 
-CREATE VIEW gold.dim_customers AS
+CREATE VIEW Gold.dim_customers AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
     ci.cst_id                          AS customer_id,
@@ -36,21 +36,21 @@ SELECT
     END                                AS gender,
     ca.bdate                           AS birthdate,
     ci.cst_create_date                 AS create_date
-FROM silver.crm_cust_info ci
-LEFT JOIN silver.erp_cust_az12 ca
+FROM Silver.crm_cust_info ci
+LEFT JOIN Silver.erp_cust_az12 ca
     ON ci.cst_key = ca.cid
-LEFT JOIN silver.erp_loc_a101 la
+LEFT JOIN Silver.erp_loc_a101 la
     ON ci.cst_key = la.cid;
 GO
 
 -- =============================================================================
--- Create Dimension: gold.dim_products
+-- Create Dimension: Gold.dim_products
 -- =============================================================================
-IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
-    DROP VIEW gold.dim_products;
+IF OBJECT_ID('Gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW Gold.dim_products;
 GO
 
-CREATE VIEW gold.dim_products AS
+CREATE VIEW Gold.dim_products AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
     pn.prd_id       AS product_id,
@@ -63,20 +63,20 @@ SELECT
     pn.prd_cost     AS cost,
     pn.prd_line     AS product_line,
     pn.prd_start_dt AS start_date
-FROM silver.crm_prd_info pn
-LEFT JOIN silver.erp_px_cat_g1v2 pc
+FROM Silver.crm_prd_info pn
+LEFT JOIN Silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
 WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
 GO
 
 -- =============================================================================
--- Create Fact Table: gold.fact_sales
+-- Create Fact Table: Gold.fact_sales
 -- =============================================================================
-IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
-    DROP VIEW gold.fact_sales;
+IF OBJECT_ID('Gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW Gold.fact_sales;
 GO
 
-CREATE VIEW gold.fact_sales AS
+CREATE VIEW Gold.fact_sales AS
 SELECT
     sd.sls_ord_num  AS order_number,
     pr.product_key  AS product_key,
@@ -87,9 +87,9 @@ SELECT
     sd.sls_sales    AS sales_amount,
     sd.sls_quantity AS quantity,
     sd.sls_price    AS price
-FROM silver.crm_sales_details sd
-LEFT JOIN gold.dim_products pr
+FROM Silver.crm_sales_details sd
+LEFT JOIN Gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
-LEFT JOIN gold.dim_customers cu
+LEFT JOIN Gold.dim_customers cu
     ON sd.sls_cust_id = cu.customer_id;
 GO
