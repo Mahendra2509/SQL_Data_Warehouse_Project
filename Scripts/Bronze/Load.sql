07cf26ec43a3e4f19e4e6628796934faca054e99
Naga Mahendra Polavarapu <mahendrachowdary36@gmail.com>
2026-02-18 19:19:34 +0530

    Update Load.sql

--- Scripts/Bronze/Load.sql
+++ Scripts/Bronze/Load.sql
@@ -1,5 +1,3 @@
-
-
 BULK INSERT Bronze.crm_cust_info
 FROM 'C:\Users\mahen\Downloads\datasets\source_crm\cust_info.csv'
 WITH (
@@ -7,8 +5,6 @@ WITH (
 	FIELDTERMINATOR = ',',
 	TABLOCK
 );
-
-
 
 BULK INSERT Bronze.crm_prd_info
 FROM 'C:\Users\mahen\Downloads\datasets\source_crm\prd_info.csv'
@@ -18,8 +14,6 @@ WITH (
 	TABLOCK
 );
 
-
-
 BULK INSERT Bronze.crm_sales_details
 FROM 'C:\Users\mahen\Downloads\datasets\source_crm\sales_details.csv'
 WITH (
@@ -27,8 +21,6 @@ WITH (
 	FIELDTERMINATOR = ',',
 	TABLOCK
 );
-
-
 
 BULK INSERT Bronze.erp_cust_az12
 FROM 'C:\Users\mahen\Downloads\datasets\source_erp\cust_az12.csv'
@@ -38,8 +30,6 @@ WITH (
 	TABLOCK
 );
 
-
-
 BULK INSERT Bronze.erp_loc_a101
 FROM 'C:\Users\mahen\Downloads\datasets\source_erp\loc_a101.csv'
 WITH (
@@ -48,8 +38,6 @@ WITH (
 	TABLOCK
 );
 
-
-
 BULK INSERT Bronze.erp_px_cat_g1v2
 FROM 'C:\Users\mahen\Downloads\datasets\source_erp\px_cat_g1v2.csv'
 WITH (
@@ -57,7 +45,3 @@ WITH (
 	FIELDTERMINATOR = ',',
 	TABLOCK
 );
-
-
-
-
